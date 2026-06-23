3fe506e68f63cf715310b1d3db04e81a0d324783
joagarridoc-design <joa.garridoc@duocuc.cl>
2026-06-22 23:20:29 -0400

    ENRIQUECIMIENTO TEST
    se le agregaron los metodos acordes según service del microservicio

--- ms-category/src/test/java/com/example/ms_category/Service/CategoryServiceTest.java
+++ ms-category/src/test/java/com/example/ms_category/Service/CategoryServiceTest.java
@@ -10,9 +10,10 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
 import java.util.Arrays;
-
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -46,11 +47,40 @@ public class CategoryServiceTest {
         verify(categoryRepository, times(1)).findById(1);
     }
     @Test
+    public void testSaveCategory() {
+        System.out.println("TEST CATEGORY: EJECUTANDO SAVE CATEGORY");
+        when(categoryRepository.save(any(Category.class))).thenReturn(categoryFalsa);
+        Category resultado = categoryService.saveCategory(categoryFalsa);
+        assertNotNull(resultado);
+        assertEquals(1, resultado.getId());
+        assertEquals("Electrónica", resultado.getName());
+        verify(categoryRepository, times(1)).save(categoryFalsa);
+    }
+    @Test
     public void testObtenerCategoryPorId_NoEncontrado() {
         System.out.println("TEST CATEGORY: EJECUTANDO POSIBILIDAD NULL");
         when(categoryRepository.findById(99)).thenReturn(Optional.empty());
         Category resultado = categoryService.findById(99);
         assertNull(resultado, "Debería retornar NULL al no existir la categoría 99");
         verify(categoryRepository, times(1)).findById(99);
+    }
+    @Test
+    public void testGetAllCategory() {
+        System.out.println("TEST CATEGORY: EJECUTANDO OBTENER TODAS LAS CATEGORIAS");
+        List<Category> listaEsperada = Arrays.asList(categoryFalsa);
+        when(categoryRepository.findAll()).thenReturn(listaEsperada);
+        List<Category> resultado = categoryService.getAllCategory();
+        assertNotNull(resultado);
+        assertFalse(resultado.isEmpty());
+        assertEquals(1, resultado.size());
+        verify(categoryRepository, times(1)).findAll();
+    }
+    @Test
+    public void testDeleteCategory() {
+        System.out.println("TEST CATEGORY: EJECUTANDO ELIMINAR CATEGORIA");
+        Integer idEliminar = 1;
+        doNothing().when(categoryRepository).deleteById(idEliminar);
+        categoryService.deleteCategory(idEliminar);
+        verify(categoryRepository, times(1)).deleteById(idEliminar);
     }
 }
