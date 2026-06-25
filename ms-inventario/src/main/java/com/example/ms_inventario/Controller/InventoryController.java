5c3e40c7bded7748c8a21b298dfb26a579c016fa
joagarridoc-design <joa.garridoc@duocuc.cl>
2026-06-25 11:16:04 -0400

    CrossOrigin(origins = "http://localhost:8080")
    añadí la línea de codigo "CrossOrigin(origins = "http://localhost:8080")" para que permita solicitudes y se comunique con API GATEWAY centralizado

--- ms-inventario/src/main/java/com/example/ms_inventario/Controller/InventoryController.java
+++ ms-inventario/src/main/java/com/example/ms_inventario/Controller/InventoryController.java
@@ -21,6 +21,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/inventarios")
+@CrossOrigin(origins = "http://localhost:8080")
 public class InventoryController {
 
     @Autowired
