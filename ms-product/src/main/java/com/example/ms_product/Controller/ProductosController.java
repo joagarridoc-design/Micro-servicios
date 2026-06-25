45a2c8f4dc2354cd11e6338279babf31a4354e6b
joagarridoc-design <joa.garridoc@duocuc.cl>
2026-06-25 11:14:00 -0400

    CrossOrigin(origins = "http://localhost:8080")
    añadí la línea de codigo "CrossOrigin(origins = "http://localhost:8080")" para que permita solicitudes y se comunique con API GATEWAY centralizado

--- ms-product/src/main/java/com/example/ms_product/Controller/ProductosController.java
+++ ms-product/src/main/java/com/example/ms_product/Controller/ProductosController.java
@@ -20,6 +20,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/productos")
+@CrossOrigin(origins = "http://localhost:8080")
 @Tag(name = "Productos", description ="Operaciones relacionadas con productos")
 public class ProductosController {
 
