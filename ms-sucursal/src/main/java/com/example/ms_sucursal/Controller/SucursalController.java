94843921756b4a9914d313b26a15419c2b87c48a
joagarridoc-design <joa.garridoc@duocuc.cl>
2026-06-25 11:09:35 -0400

    CrossOrigin(origins = "http://localhost:8080")
    añadí la línea de codigo "CrossOrigin(origins = "http://localhost:8080")" para que permita solicitudes y se comunique con API GATEWAY centralizado

--- ms-sucursal/src/main/java/com/example/ms_sucursal/Controller/SucursalController.java
+++ ms-sucursal/src/main/java/com/example/ms_sucursal/Controller/SucursalController.java
@@ -17,6 +17,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/sucursales")
+@CrossOrigin(origins = "http://localhost:8080")
 public class SucursalController {
 
     @Autowired
